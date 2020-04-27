eca904239c127e54276cba063c5b0208953d83d1
mokeaveny <michaelkeaveny0@gmail.com>
2020-04-27 21:03:48 +0100

    Working webpage. Shows article titles from an article database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Blogger::Application.routes.draw do
+	resources :articles # Tells rails we have a resource named articles and requests can be made on it
 end
